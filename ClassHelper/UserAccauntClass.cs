2917607f2e7b4e71a9dc3fcfd93450b49d1faa8f
Dezster213 <116334561+Dezster213@users.noreply.github.com>
2023-03-27 16:42:39 +0300

    Add files via upload

---
+++ ClassHelper/UserAccauntClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery_Sanin_Cheprasov.ClassHelper
+{
+    internal class UserAccauntClass
+    {
+       public static DB.UserAccount user = new DB.UserAccount();
+    }
+}
